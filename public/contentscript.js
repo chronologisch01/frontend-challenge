d2620b6ef29cc538bb75b6d0fef1d1559a2d2841
basti-home <bastianwoidt@gmail.com>
2022-11-28 12:36:09 +0100

    Finished assignment

--- public/contentscript.js
+++ public/contentscript.js
@@ -1,11 +1,77 @@
-// console.log('Contentscript injected');
+console.log('Contentscript injected');
+
+let budget = document.getElementsByClassName("makeStyles-btb-27")[0].textContent;
+console.log(budget);
+
+let div = document.createElement("div");
+let button = document.createElement("div");
+let textNode = document.createTextNode(`Budget to beat: ${budget}`);
+button.appendChild(textNode);
+button.className="budget-div";
+div.appendChild(button);
+
+// create style
+const style = document.createElement("style");
+style.textContent= `
+    .budget-div{
+        background: #063970;
+        color: #eeeee4;
+        display:block;
+        padding:10px;
+        float:right;
+        border-radius: 25px;
+        margin-right:10%;
+    }
+
+    .info{
+        display:none;
+        z-index:100000;
+        border-radius: 25px;
+        position:absolute;
+        background: #abdbe3;
+        color: #010400;
+        margin:3%;
+        margin-left:0%;
+        padding:1.5%;
+        padding-left:2%;
+        font-size:15x;
+       
+    }
+
+    .budget-div:hover + .info {
+        display:block;
+ 
+    }
+
+    
+`
+document.head.appendChild(style);
 
 // let element = document.createElement('div');
 // const injectedText = document.createTextNode(`Extension-like element`);
 
 // element.appendChild(injectedText);
 
-// const parentElement = document.querySelector(
-//   '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5 > div:nth-child(1) > div > section > div:nth-child(2) > h2',
-// );
+
+let hover_info = document.createElement("p");
+// Source: wikipedia
+hover_info.textContent="In common usage, climate change describes global warming—the ongoing increase in global average temperature—and its effects on Earth's climate system. Climate change in a broader sense also includes previous long-term changes to Earth's climate. The current rise in global average temperature is more rapid than previous changes, and is primarily caused by humans burning fossil fuels.[2][3] Fossil fuel use, deforestation, and some agricultural and industrial practices increase greenhouse gases, notably carbon dioxide and methane.[4] Greenhouse gases absorb some of the heat that the Earth radiates after it warms from sunlight. Larger amounts of these gases trap more heat in Earth's lower atmosphere, causing global warming."
+hover_info.className= "info";
+
+
+const parentElement = document.querySelector(
+       '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5 > div:nth-child(1) > div > section > div',
+ );
+
+/*parentElement.appendChild(div);
+parentElement.appendChild(hover_div);*/
+
+
+
+div.appendChild(hover_info);
+
+parentElement.insertBefore(div, parentElement.children[0]);
+
 // parentElement.appendChild(element);
+
+
--- src/tests/e2e/loginFlow.test.tsx
+++ src/tests/e2e/loginFlow.test.tsx
@@ -1,16 +1,32 @@
 /**
  * @jest-environment puppeteer
  */
-import 'expect-puppeteer';
 
-beforeEach(async () => {
-  await page.goto('http://localhost:3000/');
-});
 
-describe('super awesome end to end tests', () => {
-  jest.setTimeout(20000);
+// I have no idea how to use Jest
+// I used the following tutorial: https://blog.logrocket.com/end-to-end-testing-react-jest-puppeteer/
+// Edit: If I say I used the tutorial what I mean is I copy-pasted the first code example. 
 
-  test('if it does something', async () => {
-    // ...
+ import puppeteer from "puppeteer";
+
+
+describe("index.tsx", () => {
+  let browser:any;
+  let page:any;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
   });
+
+  jest.setTimeout(1000000);
+
+  it("contains the budget", async () => {
+    await page.goto("http://localhost:3000/extension");
+    await page.waitForSelector(".budget-div");
+    const text = await page.$eval(".budget-div", (e:any) => e.textContent);
+    expect(text).toContain("Budget to beat:  351 €");
+  });
+
+  afterAll(() => browser.close());
 });
